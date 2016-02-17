fc49d01fb5ca5fe84e9ae0d273186fb212d6aa7e
cjurden <colejurden@gmail.com>
2016-02-16 21:13:20 -0600

    created second app and began implementing

---
+++ CameraAppRevision/CameraAppRevision/ViewController.swift
@@ -0,0 +1,34 @@
+//
+//  ViewController.swift
+//  CameraAppRevision
+//
+//  Created by Nicholas Jurden on 2/16/16.
+//  Copyright © 2016 Nicholas Jurden. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    @IBOutlet var imageView: UIImageView?
+    
+    @IBAction func takePhoto(sender: UIButton) {
+        
+    }
+    
+    @IBAction func selectPhoto(sender: UIButton) {
+        
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
